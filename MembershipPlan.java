6acd2902e606034cadf6fec463f2acc5d3782df3
s35177-rgb <s35177@pjwstk.edu.pl>
2026-04-24 22:33:24 +0200

    Add MembershipPlan

---
+++ MembershipPlan.java
@@ -0,0 +1,50 @@
+public abstract class MembershipPlan implements Billable {
+
+    private String planCode;
+    private String clientName;
+    private int months;
+    private double baseMonthlyFee;
+    private boolean autoRenew;
+
+    public MembershipPlan(String planCode, String clientName, int months,
+                          double baseMonthlyFee, boolean autoRenew) {
+        this.planCode = planCode;
+        this.clientName = clientName;
+        this.months = months;
+        this.baseMonthlyFee = baseMonthlyFee;
+        this.autoRenew = autoRenew;
+    }
+
+    public String getPlanCode() { return planCode; }
+    public String getClientName() { return clientName; }
+    public int getMonths() { return months; }
+    public double getBaseMonthlyFee() { return baseMonthlyFee; }
+    public boolean isAutoRenew() { return autoRenew; }
+
+    public abstract String getPlanType();
+    public abstract double calculateMonthlyNetPrice();
+
+    @Override
+    public double calculateMonthlyGrossPrice() {
+        return calculateMonthlyNetPrice() * 1.23;
+    }
+
+    public double calculateTotalNetPrice() {
+        return calculateMonthlyNetPrice() * months;
+    }
+
+    public final void printSummary() {
+        System.out.println("Plan: " + getPlanType());
+        System.out.println("Client: " + clientName);
+        System.out.println("Monthly net: " + calculateMonthlyNetPrice());
+        System.out.println("Monthly gross: " + calculateMonthlyGrossPrice());
+        System.out.println("Total net: " + calculateTotalNetPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "Code: " + planCode +
+                ", Months: " + months +
+                ", AutoRenew: " + autoRenew;
+    }
+}
